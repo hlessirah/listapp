ccaeb36f42e4ceea8208c0adcdfb58693d4fdc94
Rebekah Weathers <hlessirah@gmail.com>
2023-09-08 14:03:54 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.set('view engine', 'ejs')
+app.use(express.static('static'))
+
+app.get("/", (req, res) => {
+  res.render('index')
+})
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT)
+})
